1e52ea0b299689a4831b6d1da6467880274e245b
Thomas Gomree <gomreethomas@gmail.com>
2023-11-06 16:37:41 +0100

    added style scss directory

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script defer src="script.js"></script>
+    <link rel="stylesheet" href="assets/css/style.min.css">
     <title>Hackers Poulette</title>
 </head>
 <body>
