07fa084c4cf733d62a32c5b3598ae5927c15fe42
Thomas Gomree <gomreethomas@gmail.com>
2023-11-06 11:25:38 +0100

    Added action to form (php)

--- index.php
+++ index.php
@@ -13,46 +13,50 @@
     
 <?php
 
-
 $error = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $lastname = htmlspecialchars($_POST['lastname']);
-    // Sanitize other fields...
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
 
     // Check if honeypot field is filled
     if (!empty($_POST['honeypot'])) {
         die('Spam detected');
     }
 
-    // Validate fields...
-    if (empty($name) || empty($lastname) /* || other fields are empty... */) {
+     // Validate fields...
+     if (empty($name) || empty($lastname) || empty($email) || empty($message)) {
         $error = 'All fields are required';
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = 'Invalid email format';
+    } elseif (preg_match('/\d/', $name) || preg_match('/\d/', $lastname)) {
+        $error = 'Name and lastname cannot contain numbers';
     }
-}
-// If no errors, send email...
-if (empty($error)) {
-    $to = 'gomreethomas@gmail.com';
-    $subject = isset($_POST['subject']) ? $_POST['subject'] : 'Other';
-    $message = $_POST['message'];
-    if(mail($to, $subject, $message)){
-        echo '<script>alert("Your request as been sent !");</script>';
-    } else {
-        $error = 'Failed to send email';
-    }
-}
 
+    // If no errors, send email...
+    if (empty($error)) {
+        $to = 'gomreethomas@gmail.com';
+        $subject = isset($_POST['subject']) ? $_POST['subject'] : 'Other';
+        if(mail($to, $subject, $message)){
+            echo '<script>alert("Your request has been sent !");</script>';
+        } else {
+            $error = 'Failed to send email';
+        }
+    }
+}
 ?>
 
 
 <form method="post" action="index.php">
 
     <label class="label" for="name">Name:</label><br>
-    <input type="text" id="name" name="name" aria-label="Name" required><br>
+    <input type="text" id="name" name="name" placeholder="Your name" aria-label="Name" required><br>
 
     <label class="label" for="lastname">Last Name:</label><br>
-    <input type="text" id="lastname" name="lastname" aria-label="Last Name" required><br>
+    <input type="text" id="lastname" name="lastname" placeholder="Your lastname" aria-label="Last Name" required><br>
 
     <label class="label" for="gender">Gender:</label><br>
     <select id="gender" name="gender" aria-label="Gender" required>
@@ -63,22 +67,21 @@ if (empty($error)) {
     </select><br>
 
     <label class="label" for="email">Email:</label><br>
-    <input type="email" id="email" name="email" aria-label="Email" required><br>
+    <input type="email" id="email" name="email" placeholder="name@xyz.com" aria-label="Email" required><br>
 
     <label class="label" for="country">Country:</label><br>
-    <input type="text" id="country" name="country" aria-label="Country" required><br>
+    <input type="text" id="country" name="country" placeholder="Your country" aria-label="Country" required><br>
 
 
     <label class="label" for="subject">Subject:</label><br>
     <select id="subject" name="subject" aria-label="Subject">
         <option value="Other">Other</option>
-        <option value="Option 1">Product</option>
-        <option value="Option 2">Delivery</option>
-        <option value="Option 3">Documentation</option>
+        <option value="Product">Product</option>
+        <option value="Delivery">Delivery</option>
     </select><br>
 
     <label class="label" for="message">Message:</label><br>
-    <textarea id="message" name="message" aria-label="Message" required></textarea><br>
+    <textarea id="message" name="message" placeholder="Your message..." aria-label="Message" required></textarea><br>
 
     <!--Honeypot field-->
     <input type="text" id="honeypot" name="honeypot" style="display: none;"><br>
