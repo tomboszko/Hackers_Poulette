8eb216e55f0fc2489d444513cafa1ef8972df51a
Thomas Gomree <gomreethomas@gmail.com>
2023-11-07 10:06:41 +0100

    added homepage

--- partials/home.php
+++ partials/home.php
@@ -1,4 +1,14 @@
 
 <div id="homeImg">
-    <img src="/assets/picture/assets/picture/hackers-poulette-logo.png" alt="logo">
+    <img src="assets/picture/hackers-poulette-logo.png" alt="logo">
+</div>
+
+<div id="homeText">
+    <h2>Who are we?</h2>
+    <p>Hackers Poulette is a company that sells Raspberry Pi kits and accessories. We are based in Belgium, but we ship worldwide.</p>
+    <p>Our mission is to provide you with the best Raspberry Pi equipment at the best price.</p>
+</div>
+
+<div id="formButton">
+    <button id="btnForm">Contact us</button>
 </div>
